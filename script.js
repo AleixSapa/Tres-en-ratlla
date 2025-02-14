af3f5712d790d869f62943aeed0e763809eebdea
AleixSaPa <sapafamilia@gmail.com>
2025-02-14 12:39:38 +0100

    crear un boto de reiniciar fer que les x i les o es vein

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var LA_X = true;
 function Quan_clicas_al_boto(Buto) {
-  Buto.textContent = "Hola";
+  console.log(Buto.textContent);
+  if (Buto.textContent != "") {
+    return;
+  }
+  if (LA_X == true) {
+    Buto.textContent = "X";
+    LA_X = false;
+  } else {
+    Buto.textContent = "O";
+    LA_X = true;
+  }
+}
+function Reiniciar() {
+  location.reload(true);
 }
